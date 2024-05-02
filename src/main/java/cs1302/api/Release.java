5a4c0e8e7fc03faf164eea0f51f2cee5fc8d2c9b
Lia Mathews <lsm66539@uga.edu>
2024-05-01 20:20:24 -0400

    added new features to Release.java

--- src/main/java/cs1302/api/Release.java
+++ src/main/java/cs1302/api/Release.java
@@ -5,4 +5,8 @@ import java.util.List;
 public class Release {
     String title;
     int date;
+
+    public String toString() {
+        return "Title: " + title + "\tRelease Date: " + date;
+    }
 }
