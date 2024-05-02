77f145dd4b71e172f857cc72f6c69c8241c4b729
Lia Mathews <lsm66539@uga.edu>
2024-05-01 20:19:50 -0400

    added new features to Artist.java

--- src/main/java/cs1302/api/Artist.java
+++ src/main/java/cs1302/api/Artist.java
@@ -5,6 +5,16 @@ import java.util.List;
 public class Artist {
 
     String name;
-    String id;
+    String country;
+    List<Tag> tags;
+
+    static class Tag {
+        String name;
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
 
 }
