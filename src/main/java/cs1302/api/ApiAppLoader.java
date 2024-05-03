6c4f03237ed7249ede781c6fcba17682f5ca4ab8
Lia Mathews <lsm66539@uga.edu>
2024-05-03 02:10:29 -0400

    Added features to ApiAppLoader.java

--- src/main/java/cs1302/api/ApiAppLoader.java
+++ src/main/java/cs1302/api/ApiAppLoader.java
@@ -22,9 +22,12 @@ import javafx.scene.control.Alert;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Arrays;
 import java.nio.charset.StandardCharsets;
-import cs1302.api.Release;
 import java.util.List;
 import javafx.geometry.Insets;
+import javafx.scene.text.Text;
+import javafx.scene.layout.Priority;
+import javafx.application.Platform;
+import javafx.scene.control.OverrunStyle;
 
 public class ApiAppLoader extends VBox {
 
@@ -49,48 +52,51 @@ public class ApiAppLoader extends VBox {
     ImageView artistImage3;
 
     Artist artist;
-    Release[] releases;
-    Release[] releaseLabel;
 
     public static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
         .version(HttpClient.Version.HTTP_2)
         .followRedirects(HttpClient.Redirect.NORMAL)
         .build();
 
-    public static final String DEFAULT_IMG = "file:resources/default.png";
+    public static final String API_KEY = "ac27f5d94abeff5961ce56b3d823e86d";
 
     public ApiAppLoader() {
         super();
 
         this.search = new HBox(8);
-        this.searchLabel = new Label("Search: ");
-        this.searchEngine = new TextField("Enter artist name");
+        this.searchLabel = new Label("Enter Artist Name: ");
+        this.searchEngine = new TextField("Search");
         this.searchButton = new Button("Go");
         this.search.getChildren().addAll(searchLabel, searchEngine, searchButton);
 
         this.artistInfo = new HBox(8);
         this.artistImage = new ImageView();
-        this.setUp = new Image(DEFAULT_IMG);
-        this.artistImage.setImage(setUp);
-        artistImage.setFitWidth(500);
-        artistImage.setFitHeight(200); // 200 + 150 + 50 for the heights
-        artistImage.setPreserveRatio(true);
+        this.artistImage.setFitWidth(550);
+        this.artistImage.setFitHeight(200); // 200 + 150 + 50 for the heights
+        this.artistImage.setPreserveRatio(true);
+
         this.releaseInfo = new TextFlow(); // create loadPage method
-        this.releaseBox = new VBox(8);
         this.releasePane = new ScrollPane(releaseInfo);
-        this.artistInfo.getChildren().addAll(artistImage, releaseBox);
+        this.releasePane.setPrefSize(200, 200);
+        this.artistInfo.getChildren().addAll(artistImage, releasePane);
+        HBox.setHgrow(releasePane, Priority.ALWAYS);
+        HBox.setHgrow(artistImage, Priority.ALWAYS);
 
         this.otherArtist = new HBox(8);
         this.artistImage1 = new ImageView(); // create loadImage method
-        this.artist1 = new Button();
+        this.artist1 = new Button("Top Tracks");
         this.artistImage2 = new ImageView();
-        this.artist2 = new Button();
+        this.artist2 = new Button("Top Albums");
         this.artistImage3 = new ImageView();
-        this.artist3 = new Button();
+        this.artist3 = new Button("Similar Artists");
         this.otherArtist.getChildren().addAll(artist1, artist2, artist3);
+        HBox.setHgrow(artist1, Priority.NEVER);
+        otherArtist.setSpacing(10);
 
-        this.releases = new Release[5];
-        this.releaseLabel = new Release[5];
+        artist1.setTextOverrun(OverrunStyle.CLIP);
+        artist2.setTextOverrun(OverrunStyle.CLIP);
+        artist3.setTextOverrun(OverrunStyle.CLIP);
+
 
         this.getChildren().addAll(search, artistInfo, otherArtist);
 
@@ -102,15 +108,15 @@ public class ApiAppLoader extends VBox {
         });
 
         artist1.setOnAction((event) -> {
-            // add method call here
+            topTracks();
         });
 
         artist2.setOnAction((event) -> {
-            // add method call here
+            topAlbums();
         });
 
         artist3.setOnAction((event) -> {
-            // add method call here
+            similarArtists();
         });
 
     }
@@ -134,21 +140,17 @@ public class ApiAppLoader extends VBox {
                 int statusCode = response.statusCode();
                 if (statusCode == 200) {
                     String json = response.body();
-                    //System.out.println(json);
                     ArtistOutput artistList  = gson.fromJson(json, ArtistOutput.class);
                     if (artistList != null && artistList.artists != null) {
-                        Artist artist = artistList.artists.get(0); // gets first artist
-                        System.out.println("Name: " + artist.name);
-                        System.out.println("Country: " + artist.country);
-                        System.out.println("Tags: ");
-
-                        for (int i = 0; i < 5; i++) { // arrange in order of count/popularity???
-                            if (artist.tags.get(i).toString() == null) {
+                        artist = artistList.artists.get(0); // gets first artist
+                        for (int i = 0; i < 5; i++) {
+                            if (artist.tags.size() < i + 1
+                                || artist.tags.get(i).toString() == null) {
                                 i = 5;
                             }
-                            loadPage();
-                            System.out.println("\t+ " + artist.tags.get(i).toString());
                         }
+                        loadPage(artist);
+                        loadImage(artist);
                     }
                 } else {
                     System.out.println("Failed to retrieve artist: " + statusCode);
@@ -161,35 +163,162 @@ public class ApiAppLoader extends VBox {
         searchButton.setDisable(false);
     }
 
+    public void topTracks() {
+        String term = URLEncoder.encode(searchEngine.getText(), StandardCharsets.UTF_8);
+        String query = "?method=artist.gettoptracks&artist=" + term + "&api_key=" + API_KEY
+            + "&format=json";
+        String createUrl = "http://ws.audioscrobbler.com/2.0/" + query;
 
-    /** public void findReleases(Artist artistName) {
         try {
-            String urlID = "https://musicbrainz.org/ws/2/release-group?artist="
-                + artistName.id // gets id from Artist class
-                + "&fmt=json";
-            HttpRequest requestRelease = HttpRequest.newBuilder()
-                .uri(URI.create(urlID))
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(createUrl))
                 .build();
-            HttpResponse<String> responseRelease = HTTP_CLIENT
-                .send(requestRelease, BodyHandlers.ofString());
-            System.out.println(responseRelease.body());
-            int statusCode2 = responseRelease.statusCode();
-            if (statusCode2 == 200) {
-                Gson gson = new Gson();
-                Release[] releases = gson // creates array of releases (5)
-                    .fromJson(responseRelease.body(), Release[].class);
-                if (releases != null && releases.length > 0) {
-                    System.out.println(releases[1].toString()); // toString()?
+            HttpResponse<String> response = HTTP_CLIENT
+                .send(request, BodyHandlers.ofString());
+            Gson gson = new Gson();
+            int statusCode = response.statusCode();
+            if (statusCode == 200) {
+                String json = response.body();
+                //System.out.println(json);
+                artist = gson.fromJson(json, Artist.class);
+                if (artist != null && artist.tracks != null) {
+                    releaseInfo.getChildren().add(new Text("\nTop Tracks: "));
+                    Text trackList;
+                    for (int i = 0; i < 5; i++) {
+                        //if (artist.tracks.size() - 1 < i) {
+                        //    i = 5;
+                        //} else {
+                        trackList = new Text("\n\t+" + artist.tracks.get(i).toString());
+                        System.out.println("\n\t+" + artist.tracks.get(i).toString());
+                        releaseInfo.getChildren().add(trackList);
+                        //}
+                    }
+                    releasePane.layout();
                 } else {
-                    System.out.println("No releases found for the artist"); // code to move to next artist on list?
+                    System.out.println("Artist is null");
                 }
             } else {
-                System.out.println("Failed to retrieve releases" + statusCode2);
+                System.out.println("Status Code error: " + statusCode);
             }
         } catch (IOException | InterruptedException e) {
-            System.err.println("Error when finding releases: ");
-            e.printStackTrace();
+            System.out.println("Cannot retrieve tracks");
+        }
+    }
+
+    public void topAlbums() {
+        String term = URLEncoder.encode(searchEngine.getText(), StandardCharsets.UTF_8);
+        String query = "?method=artist.gettopalbums&artist=" + term + "&api_key=" + API_KEY
+             + "&format=json";
+        String createUrl = "http://ws.audioscrobbler.com/2.0/" + query;
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(createUrl))
+                .build();
+            HttpResponse<String> response = HTTP_CLIENT
+                .send(request, BodyHandlers.ofString());
+            Gson gson = new Gson();
+            int statusCode = response.statusCode();
+            if (statusCode == 200) {
+                String json = response.body();
+                Artist artist = gson.fromJson(json, Artist.class);
+                if (artist != null && artist.albums != null) {
+                    releaseInfo.getChildren().add(new Text("\nTop Albums: "));
+                    Text trackList;
+                    for (int i = 0; i < 3; i++) {
+                        trackList = new Text("\n\t+" + artist.albums.get(i).toString());
+                        releaseInfo.getChildren().add(trackList);
+                    }
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Cannot retrieve albums");
         }
 
-        }*/
+    }
+
+    public void similarArtists() {
+    }
+
+    public void loadPage(Artist artistName) {
+        Text name = new Text("\n Name: " + artistName.name);
+        Text country = new Text("\n Country: " + artistName.country);
+        this.releaseInfo.getChildren().addAll(name, country);
+        this.releaseInfo.getChildren().add(new Text("\n Tags: "));
+        for (int i = 0; i < 5; i++) {
+            if (artistName.tags.size() < i + 1
+                || artistName.tags.get(i).toString() == null) { // add try catch for this
+                i = 5;
+
+            } else {
+                releaseInfo.getChildren().add(new Text("\n\t+ "
+                    + artistName.tags.get(i).toString()));
+            }
+        }
+    }
+
+    public void loadImage(Artist artistName) {
+        new Thread(() ->
+        {
+            String term = URLEncoder.encode(searchEngine.getText(), StandardCharsets.UTF_8);
+            String query = "?method=artist.getinfo&artist=" + term + "&api_key=" + API_KEY
+                + "&format=json";
+            String createUrl = "http://ws.audioscrobbler.com/2.0/" + query;
+            try {
+                HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(createUrl))
+                    .build();
+                HttpResponse<String> response = HTTP_CLIENT
+                    .send(request, BodyHandlers.ofString());
+                Gson gson = new Gson();
+                int statusCode = response.statusCode();
+                if (statusCode == 200) {
+                    String json = response.body();
+                    String imageUrl = parseJson(json);
+                    if (imageUrl != null) {
+                        Platform.runLater(() -> {
+                            artistImage.setFitHeight(200);
+                            artistImage.setFitWidth(250);
+                            artistImage.setImage(new Image(imageUrl));
+                        });
+                    } else {
+                        System.out.println("Cannot retrieve artist image url.");
+                    }
+                } else {
+                    System.out.println("2. Cannot retrieve artist image.");
+                }
+            } catch (IOException | InterruptedException e) {
+                System.err.println("1. Cannot retrieve artist image.");
+            }
+        }).start();
+    }
+
+    public void setButtonImages(String imageUrl) {
+        artist1.setPrefWidth(205);
+        artist2.setPrefWidth(205);
+        artist3.setPrefWidth(205);
+
+        Image image = new Image(imageUrl);
+
+        artistImage1.setFitHeight(100);
+        artistImage1.setFitWidth(100);
+        artistImage1.setImage(image);
+        artist1.setGraphic(artistImage1);
+
+        artistImage2.setFitHeight(100);
+        artistImage2.setFitWidth(100);
+        artistImage2.setImage(image);
+        artist2.setGraphic(artistImage2);
+
+        artistImage3.setFitHeight(100);
+        artistImage3.setFitWidth(100);
+        artistImage3.setImage(image);
+        artist3.setGraphic(artistImage3);
+    }
+
+    public String parseJson(String toParse) {
+        int begin = 8 + toParse.indexOf("#text");
+        int end = 3 + toParse.indexOf("png");
+        System.out.println(toParse.substring(begin, end));
+        return toParse.substring(begin, end);
+    }
 }
